b53f0bcd4e18870b9e798d0164f84de3520fb65f
Nayana Gharte <nayana@weboniselab.com>
2012-10-17 17:46:32 +0530

    Implement task as per instance variable video

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,7 @@
+class DemoController < ApplicationController
+  def hello
+    @array=[1,2,3,4,5]
+
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsVideos::Application.config.secret_token = '7792c4c6852e79cda6975aab982877196bc7ddc55c7ffb757cdfd165e8f1b5556e52bae460f774ce8ad53f58a9d98f3253302eb77a9ca22c835bd1f6066eed4e'
