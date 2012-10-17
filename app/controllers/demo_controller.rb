b0885129d1edccd1f7def86408ee39b1580724b5
Nayana Gharte <nayana@weboniselab.com>
2012-10-17 18:38:26 +0530

    Implement Rendering Templates video

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -2,16 +2,19 @@ class DemoController < ApplicationController
 
 
   def index
-  redirect_to :action => :other_hello
+    render 'hello'
+    #render :action => :hello
+  #redirect_to :action => :other_hello
   end
 
   def hello
    # @array=[1,2,3,4,5]
-    redirect_to("http://www.lynda.com")
+    render :text => "Hello Everyone!"
+    #redirect_to("http://www.lynda.com")
   end
 
   def other_hello
-    render :text => "Hello Everyone!"
+
   end
 
 
